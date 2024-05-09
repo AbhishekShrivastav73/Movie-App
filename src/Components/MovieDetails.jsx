2511ddf48fd0ae71d60f9d2f86ef1b648d7643ce
AbhishekShrivastav73 <abhishekshrivastav2121@gmail.com>
2024-05-10 00:33:39 +0530

    added details pages

--- src/Components/MovieDetails.jsx
+++ src/Components/MovieDetails.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { asyncloadMovie } from "../actions/MovieActions";
+import { removeInfo } from "../reducers/MovieSlice";
+import Loading from "../Components/Loading";
 
 function MovieDetails() {
-  return (
-    <div>MovieDetails</div>
-  )
+  const { info } = useSelector((state) => state.movie);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(asyncloadMovie(id));
+    return () => {
+      dispatch(removeInfo());
+    };
+  }, []);
+  return info ? (
+    <div
+      style={{
+        background: `linear-gradient(rgba(0,0,0,.2),rgba(0,0,0,.65)), url(https://image.tmdb.org/t/p/original/${info.detail.backdrop_path})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="w-full h-screen p-6 bg-zinc-900 text-zinc-200"
+    >
+      <div className="flex items-center gap-3">
+        <i 
+          onClick={() => navigate("/")}
+          class="ri-arrow-left-line text-4xl hover:text-[#6556CC] "
+        ></i>
+        <a className="text-2xl text-white" target="_blank" href={info.detail.homepage}>
+        <i class="ri-external-link-fill"></i>
+        </a>
+        <a  className="text-2xl text-white"target="_blank" href={`https://www.imdb.com/title/${info.external_ids.imdb_id}/`}>IMDB</a>
+      </div>
+    </div>
+  ) : (
+    <Loading />
+  );
 }
 
-export default MovieDetails
+export default MovieDetails;
--- src/partials/TopNav.jsx
+++ src/partials/TopNav.jsx
@@ -29,7 +29,7 @@ function TopNav() {
         </div>
         <div className="dropDown z-30 flex flex-col gap-2 overflow-auto absolute w-[63%] max-h-[50vh] bg-zinc-700 top-[90%] left-7" >
             {searchs.map((items,idx)=>(
-                <Link key={idx} className='flex items-center hover:bg-zinc-200 hover:text-zinc-900 gap-5 w-full h-[20vh] px-4' > 
+                <Link to={`/${items.media_type}/details/${items.id}`} key={idx} className='flex items-center hover:bg-zinc-200 hover:text-zinc-900 gap-5 w-full h-[20vh] px-4' > 
                 <img className='h-[10vh] w-[10vh] rounded-xl object-cover' src={`https://image.tmdb.org/t/p/original/${items.backdrop_path || items.profile_path || items.poster_path}`} alt="" />
                 <h5 className='font-semibold'>{items.original_title || items.title || items.name }</h5>
              </Link>
